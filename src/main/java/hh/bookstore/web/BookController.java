cd2c0b1083813e1ba8cec2665decd2347a89532a
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-01-30 13:13:55 +0200

    Add a Thymeleaf template for the index page.
    C2.4

--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -2,14 +2,12 @@ package hh.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class BookController {
 
 	@GetMapping("/index")
 	public String sayGreetings() {
-		return "Welcome to the bookstore!";
+		return "index";
 	}
 }
